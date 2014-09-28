c65db97d4f084c5fc980677a166fd8dd3e158817
Rodion <3923001@mail.ru>
2014-09-29 01:57:10 +0300

    Fucking tabs was replaced by double-space

--- Lesson3/OpenStruct.rb
+++ Lesson3/OpenStruct.rb
@@ -1,16 +1,16 @@
 class OpenStruct
-
-	def initialize(hash)
-		raise "constructor parameter must be a Hash object" unless hash.class == Hash
-		@hash = hash
-		@hash.each do |key, value|
-			self.define_singleton_method(key) {@hash[key]} 
-			self.define_singleton_method("#{key}=") {|value| @hash[key] = value} 
-		end
-	end
+  def initialize(hash)
+    raise "constructor parameter must be a Hash object" unless hash.class == Hash
+    @hash = hash
+    @hash.each do |key, value|
+	      self.define_singleton_method(key) {@hash[key]} 
+	      self.define_singleton_method("#{key}=") {|value| @hash[key] = value} 
+    end
+  end
 end
 
-hash = {a:2,b:2,:key => "value", :s=>3}
+hash = {a:2, b:2, :key => "value", :s=>3}
+
 os = OpenStruct.new(hash)
 os.a = "aaa"
 hash[:b] = "new_b"
